e58016d4109b1a4715f371e38b2639fdb5f0087a
wintersalmon <salmonjoon@gmail.com>
2018-03-01 18:31:54 +0900

    implement default tag search and search url change

--- django_app/hangout/urls.py
+++ django_app/hangout/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from . import views
 
-
 urlpatterns = [
-    path('', views.HangoutListView.as_view(), name='list'),
+    path('', views.HangoutDefaultListView.as_view(), name='default'),
+    path('<str:tags>', views.HangoutListView.as_view(), name='list'),
 ]
--- django_app/hangout/views.py
+++ django_app/hangout/views.py
@@ -4,31 +4,26 @@ from django.views import generic
 from hangout.models import Hangout, Tag
 
 
+class HangoutDefaultListView(generic.ListView):
+    template_name = 'hangout/hangout_list.html'
+
+    def get_queryset(self):
+        return Hangout.objects.all()
+
+
 class HangoutListView(generic.ListView):
     template_name = 'hangout/hangout_list.html'
 
-    # all
-    # def get_queryset(self):
-    #     return Hangout.objects.all()
-
-    # filter one
-    # def get_queryset(self):
-    #     if self.request.GET.get('tags'):
-    #         tags = self.request.GET.get('tags')
-    #         tag = Tag.objects.filter(word=tags)
-    #         return Hangout.objects.filter(tags__in=tag)
-    #     else:
-    #         return Hangout.objects.all()
-
-    # filter more then one
     def get_queryset(self):
-        if self.request.GET.get('tags'):
-            tags = self.request.GET.get('tags').split(',')
+        if self.kwargs['tags']:
+            tags = self.kwargs['tags'].split(',')
             tags = [Tag.objects.get(word=t) for t in tags]
-
             queryset = Hangout.objects.all()
             for tag in tags:
                 queryset = queryset.filter(tags__in=[tag])
             return queryset
         else:
             return Hangout.objects.all()
+
+    def post(self, *args, **kwargs):
+        return self.get(args, kwargs)
