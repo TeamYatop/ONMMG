092428b21b251e53a0006d90b471e37b7792caef
wintersalmon <salmonjoon@gmail.com>
2018-03-06 16:44:41 +0900

    add hangout list view tag does not exist error handle

--- django_app/hangout/views.py
+++ django_app/hangout/views.py
@@ -1,9 +1,9 @@
-from django.views import generic
+from django.views.generic import ListView
 
 from hangout.models import Hangout, Tag
 
 
-class HangoutDefaultListView(generic.ListView):
+class HangoutDefaultListView(ListView):
     template_name = 'hangout/hangout_list.html'
 
     def get_queryset(self):
@@ -13,17 +13,31 @@ class HangoutDefaultListView(generic.ListView):
         return self.get(args, kwargs)
 
 
-class HangoutListView(generic.ListView):
+class HangoutListView(ListView):
     template_name = 'hangout/hangout_list.html'
 
     def get_queryset(self):
         if self.kwargs['tags']:
-            tags = self.kwargs['tags'].split(',')
-            tags = [Tag.objects.get(word=t) for t in tags]
-            queryset = Hangout.objects.all()
-            for tag in tags:
-                queryset = queryset.filter(tags__in=[tag])
-            return queryset
+            tags = list()
+
+            words = self.kwargs['tags'].split(',')
+            for w in words:
+                try:
+                    tag = Tag.objects.get(word=w)
+                except Tag.DoesNotExist:
+                    pass
+                else:
+                    tags.append(tag)
+
+            if tags:
+                queryset = Hangout.objects.all()
+                for tag in tags:
+                    queryset = queryset.filter(tags__in=[tag])
+                return queryset
+
+            else:
+                return Hangout.objects.none()
+
         else:
             return Hangout.objects.all()
 
