a0fab447387c803ddf7017cd1b7403e3f9c9b8ea
김형준 <wintersalmon@gimhyeongjun-ui-MacBook-Pro.local>
2018-03-17 17:55:19 +0900

    improve hangout api search url

--- django_app/hangout/api_urls.py
+++ django_app/hangout/api_urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from . import api_views as views
 
 urlpatterns = [
-    path('<str:slug>', views.hangout_detail, name='detail'),
-    path('search/', views.hangout_list, name='search'),
-    path('search/<str:words>', views.hangout_search_by_tags, name='search-all-tags'),
-    path('search/<str:area_name>/<str:words>', views.hangout_search_by_area_n_tags, name='search-tags-with-area'),
+    path('detail/<str:slug>', views.hangout_detail, name='detail'),
+    path('search', views.hangout_search, name='search'),
 ]
--- django_app/hangout/api_views.py
+++ django_app/hangout/api_views.py
@@ -6,13 +6,6 @@ from hangout.serializers import HangoutSerializer
 
 
 @api_view()
-def hangout_list(request):
-    hangouts = Hangout.objects.all()
-    serializer = HangoutSerializer(hangouts, many=True)
-    return JsonResponse(serializer.data, safe=False)
-
-
-@api_view()
 def hangout_detail(request, slug):
     try:
         hangout = Hangout.objects.get(slug=slug)
@@ -24,24 +17,22 @@ def hangout_detail(request, slug):
 
 
 @api_view()
-def hangout_search_by_tags(request, words):
-    tags = convert_words_to_tags(words)
-    if tags:
-        queryset = filter_hangout_with_tags(tags)
-    else:
-        queryset = Hangout.objects.none()
+def hangout_search(request):
+    area_name = request.query_params.get('area', None)
+    words = request.query_params.get('tags', None)
 
-    serializer = HangoutSerializer(queryset, many=True)
-    return JsonResponse(serializer.data, status=201, safe=False)
+    if (area_name is None) and (words is None):
+        return HttpResponse(status=404)
 
+    queryset = Hangout.objects.all()
 
-@api_view()
-def hangout_search_by_area_n_tags(request, area_name, words):
-    area_related_tags = convert_area_name_to_tags(area_name)
-    queryset = filter_hangout_with_area_related_tags(area_related_tags)
+    if area_name:
+        area_related_tags = convert_area_name_to_tags(area_name)
+        queryset = filter_hangout_with_area_related_tags(area_related_tags, queryset=queryset)
 
-    tags = convert_words_to_tags(words)
-    queryset = filter_hangout_with_tags(tags, queryset=queryset)
+    if words:
+        tags = convert_words_to_tags(words)
+        queryset = filter_hangout_with_tags(tags, queryset=queryset)
 
     serializer = HangoutSerializer(queryset, many=True)
     return JsonResponse(serializer.data, status=201, safe=False)
@@ -64,7 +55,10 @@ def convert_words_to_tags(words):
 def filter_hangout_with_area_related_tags(area_related_tags, *, queryset=None):
     queryset = Hangout.objects.all() if queryset is None else queryset
 
-    queryset = queryset.filter(tags__in=list(area_related_tags))
+    if area_related_tags:
+        queryset = queryset.filter(tags__in=list(area_related_tags))
+    else:
+        queryset = Hangout.objects.none()
 
     return queryset
 
@@ -72,7 +66,10 @@ def filter_hangout_with_area_related_tags(area_related_tags, *, queryset=None):
 def filter_hangout_with_tags(tags, *, queryset=None):
     queryset = Hangout.objects.all() if queryset is None else queryset
 
-    for tag in tags:
-        queryset = queryset.filter(tags__in=[tag])
+    if tags:
+        for tag in tags:
+            queryset = queryset.filter(tags__in=[tag])
+    else:
+        queryset = Hangout.objects.none()
 
     return queryset
